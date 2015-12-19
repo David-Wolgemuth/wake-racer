17993f51cebba7436cfc5e77b5c75b60a4a4832c
David Wolgemuth <davidjwolgemuth@gmail.com>
2015-12-18 17:16:50 -0800

    initial commit

---
+++ application/views/base_view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/styles.css">
+</head>
+<body>
+
+<div id="container">
+    Base View
+</div>
+
+</body>
+</html>
