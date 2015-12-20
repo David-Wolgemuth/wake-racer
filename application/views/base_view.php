266a83204ea890dfeb6de9f440668f35d4b7b302
Scott Puckett <spuckett253@gmail.com>
2015-12-19 16:34:46 -0800

    added header and footer to base_view

--- application/views/base_view.php
+++ application/views/base_view.php
@@ -1,14 +1,73 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-    <link rel="stylesheet" href="<?= base_url() ?>assets/css/styles.css">
-</head>
-<body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>WakeRacer</title>
 
-<div id="container">
-    Base View
-</div>
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url('assets/css/bootstrap.min.css')?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/styles.css')?>" rel="stylesheet">
 
-</body>
+  </head>
+  <body>
+
+<!-- navbar top  begins here -->
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">WakeRacer</a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Post<span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Ranks</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">User Info<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Account</a></li>
+            <li><a href="#">Log Out</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<!-- end of nav bar top -->
+
+<!-- footer -->
+<nav class="navbar navbar-default navbar-fixed-bottom">
+  <div class="container">
+    <a class="navbar-brand" href="#">WakeRacer</a>
+  </div>
+</nav>
+<!-- end of footer -->
+
+
+
+
+
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js')?>"></script>
+  </body>
 </html>
