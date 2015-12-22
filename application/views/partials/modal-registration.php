77649ee1866975777ec7b7c64e79c580fbdee736
Scott Puckett <spuckett253@gmail.com>
2015-12-21 20:18:26 -0800

    tweeked reg form

--- application/views/partials/modal-registration.php
+++ application/views/partials/modal-registration.php
@@ -12,7 +12,7 @@
         <div class="row">
     <div class=" col-sm-10 col-sm-offset-1">
         <form action="<?= base_url("users/create") ?>" method="POST">
-            <div class="col-sm-6 col-md-6">
+            <div class="col-sm-12">
                 <div class="form-group">
                     <label for="name">First Name:</label>
                     <p class="error"><?= $this->session->flashdata('name_first-error') ?></p>
@@ -22,6 +22,21 @@
                     <label for="name">Last Name:</label>
                     <p class="error"><?= $this->session->flashdata('name_last-error') ?></p>
                     <input class="form-control" type="text" name="name_last" placeholder="Washington" value="<?= $this->session->flashdata('name_last-old') ?>">
+                </div>
+                <div class="form-group">
+                    <label for="gender">Gender</label>
+                    <p class="error"><?= $this->session->flashdata('gender-error') ?></p>
+                    <select name="gender" class="form-control">
+                        <option class="form-control" disabled selected>Gender</option>
+                        <option class="form-control" value="1">Male</option>
+                        <option class="form-control" value="0">Female</option>
+                    </select>
+                </div>
+                
+                <div class="form-group">
+                    <label>Birth date</label>
+                    <p class="error"><?= $this->session->flashdata('birthdate_type-error') ?></p>
+                    <input type="date" class="form-control" name="birthdate"  placeholder="Birth day">
                 </div>
                 <div class="form-group">
                     <label for="email">Email:</label>
@@ -38,41 +53,8 @@
                     <p class="error"><?= $this->session->flashdata('confirm_password-error') ?></p>
                     <input  class="form-control" type="password" name="confirm_password" placeholder="Confirm Password" value="<?= $this->session->flashdata('confirm_password-old') ?>">
                 </div>
-            </div><!-- end of left side -->
-            <div class="col-sm-6 col-md-6">
-                <div class="form-group">
-                    <label for="gender">Gender</label>
-                    <p class="error"><?= $this->session->flashdata('gender-error') ?></p>
-                    <select name="gender" class="form-control">
-                        <option class="form-control" disabled selected>Gender</option>
-                        <option class="form-control" value="1">Male</option>
-                        <option class="form-control" value="0">Female</option>
-                    </select>
-                </div>
-                <!-- <div class="form-group">
-                    <label for="boat_type">Boat Type</label>
-                    <p class="error"><?= $this->session->flashdata('boat_type-error') ?></p>
-                    <select  class="form-control">
-                        <option disabled selected>Boat type</option>
-                        <option value="canoe">Canoe</option>
-                        <option value="kayak">Kayak</option>
-                    </select>
-                </div> -->
-                <div class="form-group">
-                    <label>Birth date</label>
-                    <p class="error"><?= $this->session->flashdata('birthdate_type-error') ?></p>
-                    <input type="date" class="form-control" name="birthdate"  placeholder="Birth day">
-                </div>
-                <!-- <div class="form-group">
-                    <label>State</label>
-                    <p class="error"><?= $this->session->flashdata('state-error') ?></p>
-                    <input type="text" name="state" class="form-control" placeholder="State">
-                </div>
-                <div class="form-group">
-                    <label>City</label>
-                    <p class="error"><?= $this->session->flashdata('city-error') ?></p>
-                    <input type="text" name="city" class="form-control" placeholder="City">
-                </div> -->
+            
+              
             </div><!-- end of right side -->
             <div class="form-group">
                 <input class="btn btn-success form-control" type="submit" value="Register">
